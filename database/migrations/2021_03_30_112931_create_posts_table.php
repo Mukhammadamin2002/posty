4be3a197d3781a7fd3d4f44a4331526c702a5a35
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-03-30 16:41:35 +0500

    Migration Made

--- database/migrations/2021_03_30_112931_create_posts_table.php
+++ database/migrations/2021_03_30_112931_create_posts_table.php
@@ -15,6 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->text('body');
             $table->timestamps();
         });
     }
