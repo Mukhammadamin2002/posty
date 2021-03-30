5cba38d9f284b84d2528dedfb5a7cfb4abb4764b
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-03-30 09:40:10 +0500

    Registration Page Made

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,20 +3,74 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-4/12 bg-white p-6 rounded-lg">
-            <form action="{{ route('register') }} method="post">
+            <form action="{{ route('register') }}" method="POST">
+                @csrf
                 <div class="mb-4">
                     <label for="name" class="sr-only">Name</label>
-                    <input type="text" name="name" id="name" placeholder="Enter Your Name" class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+                    <input type="text" name="name" id="name" placeholder="Enter Name" 
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg 
+                    @error('name') border-red-500 @enderror" value="{{ old('name') }}">
+                    @error('name')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-4">
                     <label for="username" class="sr-only">Username</label>
-                    <input type="text" name="username" id="username" placeholder="Enter Username" class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+                    <input type="text" name="username" id="username" placeholder="Enter Username" 
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg
+                    @error('username') border-red-500 @enderror" value="{{ old('username') }}">
+                    @error('username')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
 
                 <div class="mb-4">
                     <label for="email" class="sr-only">Email</label>
-                    <input type="text" name="email" id="email" placeholder="Enter Your Email" class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+                    <input type="text" name="email" id="email" placeholder="Enter Email" 
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg
+                    @error('email') border-red-500 @enderror" value="{{ old('email') }}">
+                    @error('email')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div class="mb-4">
+                    <label for="password" class="sr-only">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Choose a password" 
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg
+                    @error('password') border-red-500 @enderror" value="">
+                    @error('password')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div class="mb-4">
+                    <label for="password_confirmation" class="sr-only">Reapeat your Password</label>
+                    <input type="password" name="password_confirmation" id="password_confirmation" 
+                    placeholder="Reapeat your password" 
+                    class="bg-gray-100 border-2 w-full p-4 rounded-lg
+                    @error('password_confirmation') border-red-500 @enderror" value="">
+                    @error('password_confirmation')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div>
+                    <button type="submit" 
+                    class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">
+                        Register
+                    </button>
                 </div>
             </form>
         </div>
