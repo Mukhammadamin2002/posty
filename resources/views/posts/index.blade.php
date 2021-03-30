fe8b9bbea84286d0cc118137bc3eae76476dc3c0
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-03-30 19:00:50 +0500

    Post Page Made

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -20,7 +20,9 @@
                 </div>
 
                 <div>
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
+                    <button type="submit" 
+                    class="bg-blue-500 text-white px-4 py-2 rounded font-medium"
+                    >Post</button>
                 </div>
                 
             </form>
