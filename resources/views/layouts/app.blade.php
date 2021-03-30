356c8c65a58f2e99596d8c0dc14bafbc43d6fe24
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-03-30 09:37:59 +0500

    Navbar Added and Have Made Responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,10 @@
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
             <li>
-                <a href="" class="p-3">Home</a>
+                <a href="/" class="p-3">Home</a>
             </li>
             <li>
-                <a href="" class="p-3">Dashboard</a>
+                <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
             </li>
             <li>
                 <a href="" class="p-3">Post</a>
@@ -25,15 +25,26 @@
         </ul>
 
         <ul class="flex items-center">
-            <li>
-                <a href="" class="p-3">Mukhammdamin-Abdullaev</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Login</a>
-            </li>
-            <li>
-                <a href="{{ route('register') }}" class="p-3">Register</a>
-            </li>
+            @auth
+                <li>
+                    <a href="" class="p-3">Mukhammdamin-Abdullaev</a>
+                </li>
+                <form action="{{ route('logout') }}" method="post" class=" inline">
+                    @csrf
+                <li>
+                    <button type="submit">Logout</button>
+                </li>
+                </form>
+            @endauth
+            
+            @guest
+                <li>
+                    <a href="{{ route('login') }}" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+            @endguest
         </ul>
     </nav>
     @yield('content')
