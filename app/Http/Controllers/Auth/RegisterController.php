97cc9a53fd25ee5de3f635927c45f4c3c66fd7d5
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-03-30 09:36:36 +0500

    Sign Up Page Made with Validation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,12 +3,33 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function index()
     {
         return view('auth/register');
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email',
+            'password' => 'required|confirmed',
+        ]);
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        Auth::attempt($request->only('email', 'password'));
+
+        return redirect()->route('dashboard');
     }
 }
