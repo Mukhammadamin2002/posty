a881b6723a05fc2ebbc6a2b9a71b0faa8fe04cdf
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-03-30 09:47:30 +0500

    Functionalities Made on Controller

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return back()->with('status', 'Invalid login details');
+        }
+
+        return redirect()->route('dashboard');
+    }
+}
