285ac7592e5e282c31eba4d9c999777e1ae5144a
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-04-02 12:00:49 +0500

    Debugging Some Page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,28 +3,30 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            <form action="{{ route('posts') }}" method="post" class="mb-4">
-                @csrf
-                <div class="mb-4">
-                    <label for="body" class="sr-only">Body</label>
-                    <textarea name="body" id="body" cols="30" rows="4" 
-                    class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body')
-                        border-red-500
-                    @enderror" placeholder="Make a Post!"></textarea>
+            @auth
+                <form action="{{ route('posts') }}" method="post" class="mb-4">
+                    @csrf
+                    <div class="mb-4">
+                        <label for="body" class="sr-only">Body</label>
+                        <textarea name="body" id="body" cols="30" rows="4" 
+                        class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body')
+                            border-red-500
+                        @enderror" placeholder="Make a Post!"></textarea>
 
-                    @error('body')
-                        <div class="text-red-500 mt-2 text-sm">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
+                        @error('body')
+                            <div class="text-red-500 mt-2 text-sm">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
 
-                <div>
-                    <button type="submit" 
-                    class="bg-blue-500 text-white px-4 py-2 rounded font-medium"
-                    >Post</button>
-                </div>
-            </form>
+                    <div>
+                        <button type="submit" 
+                        class="bg-blue-500 text-white px-4 py-2 rounded font-medium"
+                        >Post</button>
+                    </div>
+                </form>
+            @endauth
 
             @if ($posts->count())
                 @foreach ($posts as $post)
