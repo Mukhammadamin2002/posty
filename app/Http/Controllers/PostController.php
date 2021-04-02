fbb371842736e1bd655de99ceae1a9d113c1ec88
Mukhammadamin <mukhammadaminabdullaev01082002@gmail.com>
2021-04-02 10:40:26 +0500

    Single Post Page Completed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,6 +15,13 @@ class PostController extends Controller
         ]);
     }
 
+    public function show(Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
+
     public function store(Request $request)
     {
         $this->validate($request, [
